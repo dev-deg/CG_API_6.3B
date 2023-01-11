df8230e77ae6d3dfaeab38a7451ebf8be5ec9696
deg <david.deguara@mcast.edu.mt>
2023-01-11 13:28:52 +0100

    Bug with filtering

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function GenerateRandomNumber(max) {
 var reqCount = 0;
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello Loser!"));
 
 app.post("/random", (req, res) => {
   var max = 100;
@@ -20,7 +20,24 @@ app.post("/random", (req, res) => {
 });
 
 app.post("/username", (req, res) => {
-  res.send(data.usernames[GenerateRandomNumber(data.usernames.length)]);
+  var start = "";
+  var filteredUsernames;
+  if (req.query.start != null) {
+    start = req.query.start;
+  }
+
+  if (start != "") {
+    if (start.length > 3) {
+      start = start.substring(0, 3);
+    }
+    //BUG ->>
+    filteredUsernames = data.usernames.filter((u) => {
+      u.substring(0, start.length) == start;
+    });
+    res.send(filteredUsernames[GenerateRandomNumber(filteredUsernames.length)]);
+  } else {
+    res.send("");
+  }
 });
 
 app.listen(4000, () => console.log("Server is listening on port 4000"));
